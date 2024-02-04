f6a5395f586766c3361d43611a7d9e48d017a3c2
Seba <seb.znaj@gmail.com>
2024-02-04 20:58:02 +0100

    adding scripts

---
+++ add2.py
@@ -0,0 +1,11 @@
+# Python3 program to add two numbers
+num1 = input("Fist number: ")
+num2 = input("Second number: ")
+
+# Add two numbers
+# User might also enter float numbers
+sum = float(num1) + float(num2)
+
+# Display the sum
+# will print value in float
+print("\nThe sum of {0} and {1} is {2}".format(num1, num2, sum))
---
+++ add3.py
@@ -0,0 +1,14 @@
+# To define a function that take two integers
+# and return the sum of those two numbers
+def add(a,b):
+    return a+b
+
+# Inintializing the varialbes
+num1 = 10
+num2 = 5
+
+# Function calling and store the result in sum_of_twonumbers
+sum_of_twonumbers = add (num1, num2)
+
+# To print the result
+print("Sum of {0} and {1} is {2};" .format(num1, num2, sum_of_twonumbers))
