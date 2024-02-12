18dd64ed0768ed54084bda396e2dbe58b9ee967b
SebastianZnaj <seb.znaj@gmail.com>
2024-02-12 21:13:46 +0100

    Delete area_of_a_circle2.py

--- area_of_a_circle2.py
+++
@@ -1,9 +0,0 @@
-# Python 3 program to find Area of a circel
-# using inbuild library
-
-import math
-
-def area(r):
-    area = math.pi* pow(r,2)
-    return print('Area of circle is:', area)
-area(4)
